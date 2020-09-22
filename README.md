947b9a0feada3a967022a7bbcb97f292610310ec
Manuele Menozzi <manuele.menozzi@gmail.com>
2020-09-22 08:19:03 +0200

    Initial commit with first WIP

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ghost Inspector anywhere
+
+Allows to run [Ghost Inspector](https://ghostinspector.com/) tests anywhere through an [ngrok](https://ngrok.com/) tunnel.
+
+## Usage
+
+```bash
+node index.js <tests>
+```
+
+Where `<tests>` could be either a directory containing Ghost Inspector JSON tests files or a single Ghost Inspector JSON test file.
+
+
+## TODO
+
+* Allow to configure Ghost Inspector (API key and organization ID) with env vars and/or CLI arguments
+* Allow to configure ngrok tunnel with config file (but watchout for [this issue](https://github.com/bubenshchykov/ngrok/issues/197)
+* Better handling of failed test (dump debug info somewhere)
+* Allow to install through npm
---
+++ index.js
@@ -0,0 +1,99 @@
+// TODO put these stuff in env file
+const GhostInspectorApiKey = ''; // Enter Ghost Inspector API Key here
+const GhostInspectorOrganizationId = ''; // Enter Ghost Inspector organization ID here
+
+const program = require('commander');
+const fs = require('fs');
+const ngrok = require('ngrok');
+const ora = require('ora');
+const chalk = require('chalk');
+const GhostInspector = require('ghost-inspector')(GhostInspectorApiKey);
+
+var findAllJsonFilesRecursively = function(dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) { 
+            /* Recurse into a subdirectory */
+            results = results.concat(findAllJsonFilesRecursively(file));
+        } else if (file.toLowerCase().endsWith('.json')) { 
+            /* Is a file */
+            results.push(file);
+        }
+    });
+    return results;
+}
+
+async function executeGhostInspectorTest (myOrganizationId, myTest) {
+    // we pass the `wait: true` option to wait for test completion
+    const result = await GhostInspector.executeTestOnDemand(myOrganizationId, myTest, { wait: true });
+    return result;
+}
+
+async function openNgrokTunnel() {
+    const spinner = ora('Creating ngrok tunnel...').start();
+    // TODO make tunnel name and config path configurable
+    const ngrokUrl = await ngrok.connect({
+        addr: '8000',
+        proto: 'http'
+    });
+    spinner.succeed(`ngrok tunnel successfully opened at "${chalk.green(ngrokUrl)}"`);
+    return ngrokUrl;
+}
+
+async function closeNgrokTunnel() {
+    const spinner = ora('Closing ngrok tunnel...').start();
+    await ngrok.disconnect();
+    await ngrok.kill();
+    spinner.succeed('ngrok tunnel closed');
+}
+
+async function mainCommandHandler(tests) {
+    try {    
+        var exitCode = 0;
+        
+        if (fs.lstatSync(tests).isDirectory) {
+            tests = findAllJsonFilesRecursively(tests);
+        } else {
+            tests = [tests];
+        }
+
+        console.log(`Found ${chalk.green(tests.length)} test(s) files.`);
+
+        if (tests.length === 0) {
+            return exitCode;
+        }
+
+        const ngrokUrl = await openNgrokTunnel();
+
+        for (testFile of tests) {
+            const testObject = require('./' + testFile);
+            testObject.startUrl = ngrokUrl;
+            const spinner = ora(`Running test "${chalk.green(testObject.name)}"...`).start();
+            const result = await executeGhostInspectorTest(GhostInspectorOrganizationId, testObject);
+            if (result.passing) {
+                spinner.succeed(`Test "${chalk.green(testObject.name)}" passed`); 
+            } else {
+                exitCode = 1;
+                spinner.fail(`Test "${chalk.green(testObject.name)}" failed`); 
+                // TODO bettere handling of failing test
+                console.log(result);
+            }
+        }
+        
+        await closeNgrokTunnel();
+    } catch (e) {
+        exitCode = 1;
+        console.error(e);
+    }
+
+    return exitCode;
+}
+
+program
+    .arguments('<tests>')
+    .action(mainCommandHandler);
+
+program.parse(process.argv);
