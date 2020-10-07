81e7d23e4d6ddc1aecc9d44a56bd66ec07c70408
Manuele Menozzi <manuele.menozzi@gmail.com>
2020-10-07 10:28:09 +0200

    Allow to run a setup and teardown script before and after tests

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const path = require('path');
 const ngrok = require('ngrok');
 const ora = require('ora');
 const chalk = require('chalk');
+const spawn = require('child_process');
 const GhostInspector = require('ghost-inspector')(GhostInspectorApiKey);
 
 var findAllJsonFilesRecursively = function(dir) {
@@ -18,10 +19,10 @@ var findAllJsonFilesRecursively = function(dir) {
     list.forEach(function(file) {
         file = dir + '/' + file;
         var stat = fs.statSync(file);
-        if (stat && stat.isDirectory()) { 
+        if (stat && stat.isDirectory()) {
             /* Recurse into a subdirectory */
             results = results.concat(findAllJsonFilesRecursively(file));
-        } else if (file.toLowerCase().endsWith('.json')) { 
+        } else if (file.toLowerCase().endsWith('.json')) {
             /* Is a file */
             results.push(file);
         }
@@ -63,7 +64,7 @@ async function mainCommandHandler(tests, outputDir) {
 
         var exitCode = 0;
         tests = path.resolve(process.cwd(), tests);
-        
+
         if (fs.lstatSync(tests).isDirectory) {
             tests = findAllJsonFilesRecursively(tests);
         } else {
@@ -78,27 +79,50 @@ async function mainCommandHandler(tests, outputDir) {
 
         const ngrokUrl = await openNgrokTunnel();
 
+        if (program.setupScript) {
+            const spinner = ora(`Executing setup script "${program.setupScript}"...`).start();
+            try {
+                await spawn.execFile(program.setupScript, [ngrokUrl]);
+            } catch (e) {
+                spinner.fail(`Setup script "${program.setupScript}" failed.`);
+                throw e;
+            }
+            spinner.succeed(`Successfully executed setup script "${program.setupScript}".`);
+        }
+
         for (testFile of tests) {
             const testObject = JSON.parse(fs.readFileSync(testFile));
             testObject.startUrl = ngrokUrl;
             const spinner = ora(`Running test "${chalk.green(testObject.name)}"...`).start();
             const result = await executeGhostInspectorTest(GhostInspectorOrganizationId, testObject);
             if (result.passing) {
-                spinner.succeed(`Test "${chalk.green(testObject.name)}" passed`); 
+                spinner.succeed(`Test "${chalk.green(testObject.name)}" passed`);
             } else {
                 exitCode = 1;
                 spinner.fail(`Test "${chalk.green(testObject.name)}" failed`);
             }
-            
+
             const now = new Date();
             const outputFilename = `${outputDir}/${now.getFullYear()}${now.getMonth()+1}${now.getDate()}${now.getHours()}${now.getMinutes()}${now.getSeconds()} - ${testObject.name}.json`;
             fs.writeFileSync(outputFilename, JSON.stringify(result, null, 2));
         }
-        
+
+        if (program.teardownScript) {
+            const spinner = ora(`Executing tear down script "${program.teardownScript}"...`).start();
+            try {
+                await spawn.execFile(program.teardownScript, [ngrokUrl]);
+            } catch (e) {
+                spinner.fail(`Tear down script "${program.teardownScript}" failed.`);
+                throw e;
+            }
+            spinner.succeed(`Successfully executed tear down script "${program.teardownScript}".`);
+        }
+
         await closeNgrokTunnel();
     } catch (e) {
         exitCode = 1;
         console.error(e);
+        process.exit(1);
     }
 
     return exitCode;
@@ -106,6 +130,8 @@ async function mainCommandHandler(tests, outputDir) {
 
 program
     .arguments('<tests> <outputDir>')
+    .option('--setup-script <script>', 'Set up script to run before running all tests. The ngrok tunnel URL will be passed as first argument.')
+    .option('--teardown-script <script>', 'Tear down script to run after running all tests.')
     .action(mainCommandHandler);
 
 program.parse(process.argv);
